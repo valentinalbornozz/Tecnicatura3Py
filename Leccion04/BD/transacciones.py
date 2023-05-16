89bb2c041be42a40e7b6193dfb4b005d6a0d4cde
Valentin Albornoz <valeg486@gmail.com>
2023-05-15 22:18:25 -0300

    Clase 6
     commitemos Transacciones en Postgresql y Python

---
+++ Leccion04/BD/transacciones.py
@@ -0,0 +1,19 @@
+import psycopg2 #Esto es para poder conectactos a postgre
+
+conexion = psycopg2.connect(user='postgres', password='1597532', host='127.0.0.1', port='5432', database='test_bd'
+)
+try:
+    #conexion.autocommit = False #esto directamente no deberia estar
+    cursor = conexion.cursor();
+    sentencia = 'INSERT INTO persona(nombre, apellido, email) VALUES (%s, %s, %s)'    
+    valores = ('María', 'Esperanza', 'mesparza@gmail.com')
+    cursor.execute(sentencia, valores)
+    conexion.commit() #Hacemos el commit manualmente
+    print('Termina la transacción')
+
+except Exception as e:
+    conexion.rollback()
+    print(f'Ocurrio un error, se hizo un rollback: {e}')
+finally:
+    conexion.close()
+
---
+++ Leccion04/BD/transacciones_dos.py
@@ -0,0 +1,23 @@
+import psycopg2 #Esto es para poder conectactos a postgre
+
+conexion = psycopg2.connect(user='postgres', password='1597532', host='127.0.0.1', port='5432', database='test_bd')
+try:
+    conexion.autocommit = False #esto directamente no deberia estar, inicia la transacción 
+    cursor = conexion.cursor();
+    sentencia = 'INSERT INTO persona(nombre, apellido, email) VALUES (%s, %s, %s)'    
+    valores = ('Jorge', 'Prol', 'jprol@gmail.com')
+    cursor.execute(sentencia, valores)
+
+    sentencia = 'UPDATE persona SET nombre=%s, apellido=%s, email=%s WHERE id_persona=%s'
+    valores = ('Juan Carlos', 'Perez', 'jcperez@gmail.com')
+    cursor.execute(sentencia, valores)
+
+
+    conexion.commit() #Hacemos el commit manualmente, se cierra la transacción
+    print('Termina la transacción')
+except Exception as e:
+    conexion.rollback()
+    print(f'Ocurrio un error, se hizo un rollback: {e}')
+finally:
+    conexion.close()
+
