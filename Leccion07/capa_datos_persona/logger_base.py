f192c7e593a35fef9e0017dcebeba5296964a681
Valentin Albornoz <valeg486@gmail.com>
2023-06-26 20:35:30 -0300

    actualización lección 07 clase 11

--- Leccion07/capa_datos_persona/logger_base.py
+++ Leccion07/capa_datos_persona/logger_base.py
@@ -7,10 +7,11 @@ log.basicConfig(level=log.DEBUG,
                 format='%(asctime)s:%(levelname)s [%(filename)s:%(lineno)s] %(message)s',
                 datefmt='%I:%M:%S %p',
                 handlers=[
-                    log.FileHandler('capa_datos.log'),
+                    log.FileHandler('capa_datos.log', encodig='utf8'),
                     log.StreamHandler()
                 ])
 if __name__ == '__main__' :
+    #todos los niveles 
     log.debug('Mensaje a nivel debug')                 
     log.info('Mensaje a nivel info')           
     log.warning('Mensaje a nivel error')           
